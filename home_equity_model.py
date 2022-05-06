c1be2c7929c2b6f6f074591126456a601ba4412d
joshuaokungbowa <okungbowa.josh@gmail.com>
2022-05-06 20:52:13 +0100

    Commented code with tasks

--- home_equity_model.py
+++ home_equity_model.py
@@ -1,5 +1,5 @@
-from enum import Enum
-
+# create the same model for the other site, the InputModels are identical, you'll need to go through the other
+# govhomeprog site to get all the xpaths for the xpath model
 
 class HomeEqXpathModel:
     # Loan Type
